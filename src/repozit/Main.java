269112abdc1af73604f9f2de37fb6039105c3f4b
21602 <21602@1k129s5.dune.edu>
2022-02-24 14:50:35 +0200

    repozit

--- src/repozit/Main.java
+++ src/repozit/Main.java
@@ -11,11 +11,9 @@ package repozit;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+      
     }
     
 }
