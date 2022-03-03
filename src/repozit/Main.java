bf10ecb3c15c53b10e12b83736792e0aba8e6a9c
21602 <21602@1k129s5.dune.edu>
2022-03-03 15:23:14 +0200

    ya_ne_frick

--- src/repozit/Main.java
+++ src/repozit/Main.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ */    //privet vsem ya ne frick
 package repozit;
 
 
@@ -12,5 +12,4 @@ public class Main {
     public static void main(String[] args) {
       
     }
-    //privet vsem ya ne frick
 }
